3179aeb6bc5d34dea15fe14709a1d5b25ebf9502
scienceduckling <scienceduckling@gmail.com>
2019-06-28 15:46:36 +0200

    allowing to choose upper limit for guessing interval

--- Guessthenumber.py
+++ Guessthenumber.py
@@ -1,32 +1,39 @@
 import random
-import sys
+
 
 def gameplay():
-    '''Guess the number game'''
-    number = random.randint(1, 1001)
-    guesses = 0
-    print("Guess our whole number between 1 and 1000\n")
+    """Guess the number game"""
+
     while True:
         try:
-            player_guess = int(input("Enter your guess here:"))
-            if player_guess == number:
-                print("\nCorrect! You win!")
-                guesses = guesses + 1
-                break
-            elif player_guess > number:
-                print("\nWrong! The answer is lower than that.")
-                guesses = guesses + 1
-            elif player_guess < number:
-                print("\nWrong! The answer is higher than that.")
-                guesses = guesses + 1
+            upper_limit = int(input("Enter upper limit for interval you want to guess from. Low limit will be 1. It has to be whole number.: "))
         except ValueError:
-            print("\nThat's not a whole number, try again.")
-            continue
-    print('You got it in', guesses, 'guesses!\n')
+            print("That's not a whole number. We will use 100 as upper limit.")
+            upper_limit = 100
 
-gameplay()
-playagain = str(input("Play again? (yes or no)\n"))
-if playagain == 'yes':
+        number = random.randint(1, upper_limit + 1)
+        guesses = 0
+        print("Guess our whole number between 1 and {}".format(upper_limit))
+        while True:
+            try:
+                player_guess = int(input("Enter your guess here: "))
+                guesses = guesses + 1
+                if player_guess == number:
+                    print("Correct! You win!")
+                    break
+                elif player_guess > number:
+                    print("Wrong! The answer is lower than that.")
+                elif player_guess < number:
+                    print("Wrong! The answer is higher than that.")
+            except ValueError:
+                print("That's not a whole number, try again.")
+                continue
+        print('You got it in', guesses, 'guesses!')
+
+        play_again = str(input("Play again? (y/n): "))
+        if play_again != 'y':
+            break
+
+
+if __name__ == "__main__":
     gameplay()
-else:
-    sys.exit
